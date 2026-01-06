86dfd24e0462561fcf58a00a8c7343b40c661b18
mary <maryam.nokohan@gmail.com>
2026-01-07 01:39:37 +0330

    rename : the main file to main.go

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	path := ""
+	fmt.Print("Enter a directory path you want to organize : ")
+	fmt.Scan(&path)
+
+	files, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatal("Can't Read Dir : ", err)
+	}
+	types := ""
+	fmt.Print("Enter the file type : ")
+	fmt.Scan(&types)
+
+	folderName := ""
+	fmt.Print("Enter a folder name to move all the " + types + " to it :")
+	fmt.Scan(&folderName)
+
+	destDir := path + "/" + folderName
+
+	err = os.Mkdir(destDir, 0750)
+	if err != nil {
+		log.Fatal("Can't create the directory : ", err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		if filepath.Ext(file.Name()) == "."+types {
+			oldPath := filepath.Join(path, file.Name())
+			newPath := filepath.Join(destDir, file.Name())
+			err := os.Rename(oldPath, newPath)
+			if err != nil {
+				log.Fatal("Failed to move file : ", err)
+			}
+		}
+	}
+
+	fmt.Println("==================100 Done")
+
+}
